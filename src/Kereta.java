90fff1e3e17632eb753c31b7d2a70e29b115a382
Dclint <padangbasudewa4@gmail.com>
2018-11-04 20:40:58 +0700

    penambahan attribute pda Kereta

---
+++ src/Kereta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Kereta {
+    
+    private String namaKereta;
+    private int gerbong;
+    
+    public Kereta (){
+        
+    }
+}
