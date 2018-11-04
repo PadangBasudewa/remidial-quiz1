3fbeba30f83d1e62d06ce307e1e748fe829a7a2f
Dclint <padangbasudewa4@gmail.com>
2018-11-04 20:48:34 +0700

    penambahan attribute pda class Tiket

---
+++ src/Tiket.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Tiket {
+    
+    private String tujuan;
+    private int noDuduk;
+    private double hargaTiket;
+    
+    private Tiket(){
+        
+    }
+    
+}
